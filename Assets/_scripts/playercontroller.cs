36b05613826f1a04b829aa8adc1084b67df27bc8
dustinsison <dustin.sison@moovweb.com>
2019-06-03 16:42:41 -0700

    Lesson 2.2 Partial

---
+++ Assets/_scripts/playercontroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+	void Start()
+	{
+		
+	}
+    void Update()
+    {
+        float.moveHorizontal = Input.GetAxis ("Horizontal");
+        float.moveVertical = Input.GetAxis ("Vertical");
+
+        Rigidbody.AddForce()
+    }
+}
