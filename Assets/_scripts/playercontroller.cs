1936a4ad13be0f5f56317ce14666b19571e79894
dustinsison <dustin.sison@moovweb.com>
2019-06-04 01:08:53 -0700

    Step 3.1 Completed

--- Assets/_scripts/playercontroller.cs
+++ Assets/_scripts/playercontroller.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playercontroller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-	void Start()
-	{
-		
-	}
-    void Update()
-    {
-        float.moveHorizontal = Input.GetAxis ("Horizontal");
-        float.moveVertical = Input.GetAxis ("Vertical");
 
-        Rigidbody.AddForce()
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
     }
 }
